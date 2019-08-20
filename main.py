c08c6c7bb75cf6b5341cab35ad88620ec8d9ed41
taraalma <19585057+taraalma@users.noreply.github.com>
2019-08-20 16:12:13 -0600

    -Adding new changes...blahhhh

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 print("hello, Semestre i students")
 print("welcome to the amazing world of SVN!")
+
+# this is a comment
+
+# this is a comment as well
+
+# stop committing useless comments!
+
+print("stop committing useless comments!")
+
+
